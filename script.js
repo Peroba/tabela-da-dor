0ceaec80b976dbf91c65de8cb8d1801c96d85b69
Peroba <95951215+Peroba@users.noreply.github.com>
2025-03-13 00:55:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,97 +2,214 @@ const chat = document.getElementById('chat');
 
 const tabelasNormal = {
     bludgeoning: [
-        { resultado: "Perna Quebrada", descricao: "" },
-        { resultado: "Braço Quebrado", descricao: "" },
-        { resultado: "Costela Quebrada", descricao: "" },
-        { resultado: "Hematoma Interno", descricao: "" },
-        { resultado: "Desorientado", descricao: "" },
-        { resultado: "Visão Borrada", descricao: "" },
-        { resultado: "Amnésia Temporária", descricao: "" }
+        { resultado: "Perna Quebrada", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Braço Quebrado", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Costela Quebrada", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Hematoma Interno", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Desorientado", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Visão Borrada", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Amnésia Temporária", descricao: "Paulo dá a bunda", peso: 1 }
     ],
     piercing: [
-        { resultado: "Músculo da Perna Rasgado", descricao: "" },
-        { resultado: "Músculo do Braço Rasgado", descricao: "" },
-        { resultado: "Órgão Rompido", descricao: "" },
-        { resultado: "Hematoma Interno", descricao: "" },
-        { resultado: "Pulmão Perfurado", descricao: "" }
+        { resultado: "Músculo da Perna Rasgado", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Músculo do Braço Rasgado", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Órgão Rompido", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Hematoma Interno", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Pulmão Perfurado", descricao: "Paulo dá a bunda", peso: 1 }
     ],
     slashing: [
-        { resultado: "Lesão Ocular", descricao: "" },
-        { resultado: "Tendões Rompidos", descricao: "" },
-        { resultado: "Hematoma Interno", descricao: "" },
-        { resultado: "Pulmão Rasgado", descricao: "" },
-        { resultado: "Cicatriz Parcial", descricao: "" }
+        { resultado: "Lesão Ocular", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Tendões Rompidos", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Hematoma Interno", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Pulmão Rasgado", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Cicatriz Parcial", descricao: "Paulo dá a bunda", peso: 1 }
     ],
     acid: [
-        { resultado: "Ossos Expostos", descricao: "" },
-        { resultado: "Cegueira Parcial", descricao: "" },
-        { resultado: "Neuralgia", descricao: "" },
-        { resultado: "Desfiguração Facial", descricao: "" },
-        { resultado: "Pé Destruído", descricao: "" },
-        { resultado: "Mão Destruída", descricao: "" },
-        { resultado: "Bolhas na Pele", descricao: "" }
+        { resultado: "Ossos Expostos", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Cegueira Parcial", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Neuralgia", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Desfiguração Facial", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Pé Destruído", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Mão Destruída", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Bolhas na Pele", descricao: "Paulo dá a bunda", peso: 1 }
     ],
     cold: [
-        { resultado: "Hipotermia", descricao: "" },
-        { resultado: "Congelamento Parcial", descricao: "" },
-        { resultado: "Choque Térmico", descricao: "" },
-        { resultado: "Pé Congelado", descricao: "" },
-        { resultado: "Mão Congelada", descricao: "" },
-        { resultado: "Neuralgia", descricao: "" },
-        { resultado: "Danos Celulares", descricao: "" }
+        { resultado: "Hipotermia", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Congelamento Parcial", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Pé Congelado", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Mão Congelada", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Neuralgia", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Danos Celulares", descricao: "Paulo dá a bunda", peso: 1 }
+    ],
+    fire: [
+        { resultado: "Insolação", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Queimadura de Primeiro Grau", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Queimadura de Segundo Grau", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Neuralgia", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Bolhas", descricao: "Paulo dá a bunda", peso: 1 }
+    ],
+    force: [
+        { resultado: "Vulnerabilidade Mágica", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Doença Arcana", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Cegueira para o Mundano", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Desorientado", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Desordem Interna", descricao: "Paulo dá a bunda", peso: 1 }
+    ],
+    lightning: [
+        { resultado: "Fibrilação", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Espasmos Musculares", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Queimadura Parcial", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Lesão Cerebral", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Queimação Elétrica", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Lesão Cardíaca", descricao: "Paulo dá a bunda", peso: 1 }
+    ],
+    necrotic: [
+        { resultado: "Perda de Sangue Menor", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Mão Podre", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Pé Podre", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Músculos Podres", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Envelhecimento Prematuro", descricao: "Paulo dá a bunda", peso: 1 }
+    ],
+    poison: [
+        { resultado: "Náusea", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Vertigem", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Dano Sistêmico", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Lesão Cardíaca", descricao: "Paulo dá a bunda", peso: 1 }
+    ],
+    psychic: [
+        { resultado: "Loucura de Curto Prazo", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Enxaqueca", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Volume Impróprio", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Lesão Cerebral", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Amnésia Temporária", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Danos Cerebrais Menores", descricao: "Paulo dá a bunda", peso: 1 }
+    ],
+    radiant: [
+        { resultado: "Cegueira Parcial", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Doença de Radiação", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Queimação Radiante", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Bolhas", descricao: "Paulo dá a bunda", peso: 1 }
+    ],
+    thunder: [
+        { resultado: "Enxaqueca", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Vertigem", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Surdez Parcial", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Lesão Cerebral", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Desorientado", descricao: "Paulo dá a bunda", peso: 1 }
     ]
 };
 
 const tabelasMaior = {
     bludgeoning: [
-        { resultado: "Amnésia Permanente", descricao: "" },
-        { resultado: "Perda de Braço / Mão", descricao: "" },
-        { resultado: "Perda de Perna / Pé", descricao: "" },
-        { resultado: "Hematoma Interno", descricao: "" },
-        { resultado: "Perda de Olho", descricao: "" },
-        { resultado: "Costela Quebrada", descricao: "" },
-        { resultado: "Inconsciência Temporária", descricao: "" },
-        { resultado: "Perda de Audição", descricao: "" }
+        { resultado: "Amnésia Permanente", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Braço / Mão", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Perna / Pé", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Hematoma Interno", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Olho", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Costela Quebrada", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Inconsciência Temporária", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Audição", descricao: "Paulo dá a bunda", peso: 1 }
     ],
     piercing: [
-        { resultado: "Perda de Perna / Pé", descricao: "" },
-        { resultado: "Perda de Braço / Mão", descricao: "" },
-        { resultado: "Perda de Olho", descricao: "" },
-        { resultado: "Sangramento Brutal", descricao: "" },
-        { resultado: "Hematoma Interno", descricao: "" },
-        { resultado: "Perda de Audição", descricao: "" },
-        { resultado: "Perda de Dedos", descricao: "" },
-        { resultado: "Desfiguração Facial", descricao: "" },
-        { resultado: "Músculo Rasgado Braço / Perna", descricao: "" }
+        { resultado: "Perda de Perna / Pé", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Braço / Mão", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Olho", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Sangramento Brutal", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Hematoma Interno", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Audição", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Dedos", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Desfiguração Facial", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Músculo Rasgado Braço / Perna", descricao: "Paulo dá a bunda", peso: 1 }
     ],
     slashing: [
-        { resultado: "Perda de Dedos", descricao: "" },
-        { resultado: "Desfiguração Horrível", descricao: "" },
-        { resultado: "Perda de Perna / Pé", descricao: "" },
-        { resultado: "Perda de Braço / Mão", descricao: "" },
-        { resultado: "Perda de Olho", descricao: "" },
-        { resultado: "Perda de Audição", descricao: "" },
-        { resultado: "Sangramento Brutal", descricao: "" }
+        { resultado: "Perda de Dedos", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Desfiguração Horrível", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Perna / Pé", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Braço / Mão", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Olho", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Audição", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Sangramento Brutal", descricao: "Paulo dá a bunda", peso: 1 }
     ],
 
     acid: [
-        { resultado: "Perda de Dedos", descricao: "" },
-        { resultado: "Desfiguração Horrível", descricao: "" },
-        { resultado: "Perda de Braço / Mão", descricao: "" },
-        { resultado: "Perda de Perna / Pé", descricao: "" },
-        { resultado: "Perda de Olho", descricao: "" },
-        { resultado: "Órgãos Expostos", descricao: "" },
-        { resultado: "Perda de Audição", descricao: "" }
+        { resultado: "Perda de Dedos", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Desfiguração Horrível", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Braço / Mão", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Perna / Pé", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Olho", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Órgãos Expostos", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Audição", descricao: "Paulo dá a bunda", peso: 1 }
     ],
     cold: [
-        { resultado: "Congelamento Completo", descricao: "" },
-        { resultado: "Perda de Audição", descricao: "" },
-        { resultado: "Córneas Congeladas", descricao: "" },
-        { resultado: "Cordas Vocais Congeladas", descricao: "" },
-        { resultado: "Descarga Térmica", descricao: "" }
+        { resultado: "Congelamento Completo", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Audição", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Córneas Congeladas", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Cordas Vocais Congeladas", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Descarga Térmica", descricao: "Paulo dá a bunda", peso: 1 }
+    ],
+    fire: [
+        { resultado: "Perda de Braço / Mão", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Perna / Pé", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Olho", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Queimadura de Terceiro Grau", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Queimadura de Quarto Grau", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Desfiguração Horrível", descricao: "Paulo dá a bunda", peso: 1 }
+    ],
+    force: [
+        { resultado: "Rasgo de Alma", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Banimento", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Magia Selvagem", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Deficiência Arcana", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Colapso Corporal", descricao: "Paulo dá a bunda", peso: 1 }
+    ],
+    lightning: [
+        { resultado: "Sistema Nervoso Destruído", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Parada Cardíaca", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Lesão Cardíaca", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Desfiguração Horrível", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Braço / Mão", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Perna / Pé", descricao: "Paulo dá a bunda", peso: 1 }
+    ],
+    necrotic: [
+        { resultado: "Carne em Decadência", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Braço / Mão", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Perna / Pé", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Grande Perda de Sangue", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Envelhecimento Descontrolado", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Necrose de Órgãos", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Desfiguração Horrível", descricao: "Paulo dá a bunda", peso: 1 }
+    ],
+    poison: [
+        { resultado: "Parada Cardíaca", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Dano Grave no Fígado", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Insuficiência Renal Grave", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Lesão Cardíaca Grave", descricao: "Paulo dá a bunda", peso: 1 }
+    ],
+    psychic: [
+        { resultado: "Loucura Indefinida", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Loucura de Longo Prazo", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Fobia", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Amnésia Permanente", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Olfato", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Audição", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Apagão Cerebral", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Visão", descricao: "Paulo dá a bunda", peso: 1 }
+    ],
+    radiant: [
+        { resultado: "Câncer", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Mudança de Alinhamento", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Desfiguração Horrível", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Amaldiçoado", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Queda de Cabelo", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Cegueira", descricao: "Paulo dá a bunda", peso: 1 }
+    ],
+    thunder: [
+        { resultado: "Surdez", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Perda de Olho", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Hematoma Interno", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Pulmão Colapsado", descricao: "Paulo dá a bunda", peso: 1 },
+        { resultado: "Ossos Esmigalhados", descricao: "Paulo dá a bunda", peso: 1 }
     ]
-
 };
 
 function alternarCheckbox(checkbox) {
@@ -132,8 +249,20 @@ function sincronizarCheckboxes() {
 sincronizarCheckboxes();
 
 function sortearResultado(tabela) {
-    const indice = Math.floor(Math.random() * tabela.length);
-    return tabela[indice];
+    // Calcula o peso total da tabela
+    const pesoTotal = tabela.reduce((acc, item) => acc + item.peso, 0);
+
+    // Gera um número aleatório entre 0 e o peso total
+    const random = Math.random() * pesoTotal;
+
+    // Encontra o item correspondente ao número aleatório
+    let acumulado = 0;
+    for (const item of tabela) {
+        acumulado += item.peso;
+        if (random < acumulado) {
+            return item;
+        }
+    }
 }
 
 function adicionarRegistro(tipo, resultado, descricao) {
